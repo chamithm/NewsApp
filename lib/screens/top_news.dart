a15f907a60feee859c4f2e3253999334f6ba4db5
Chamith Madushan <112478604+chamithm1994@users.noreply.github.com>
2024-03-07 15:17:10 +0530

    Developed news details page

--- lib/screens/top_news.dart
+++ lib/screens/top_news.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
+import 'news_details.dart';
+
 class TopNews extends StatefulWidget {
   Map topNews;
   String title;
@@ -48,92 +50,97 @@ class _TopNewsState extends State<TopNews> {
             itemBuilder: (context,index){
               return Padding(
                 padding: const EdgeInsets.only(left: 14.0,right: 14.0,top: 4.0,bottom: 4.0),
-                child: Stack(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: 120,
-                      child: widget.topNews['articles'][index]['urlToImage'] != null ? ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          widget.topNews['articles'][index]['urlToImage'],fit: BoxFit.fitWidth,
-                          loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                            if (loadingProgress == null) return child;
-                            return Center(
-                              child: CircularProgressIndicator(
-                                color: Colors.blue,
-                                value: loadingProgress.expectedTotalBytes != null
-                                    ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                                    : null,
-                              ),
-                            );
-                          },
-                          errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-                            return const Center(child: Text('Failed to load image.'));
-                          },
-                        ),
-                      ) : const SizedBox(),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 120,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[900]!.withOpacity(0.6),
-                        borderRadius: BorderRadius.circular(10),
+                child: InkWell(
+                  onTap: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => NewsDetails(widget.topNews['articles'][index])));
+                  },
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        height: 120,
+                        child: widget.topNews['articles'][index]['urlToImage'] != null ? ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            widget.topNews['articles'][index]['urlToImage'],fit: BoxFit.fitWidth,
+                            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                              if (loadingProgress == null) return child;
+                              return Center(
+                                child: CircularProgressIndicator(
+                                  color: Colors.blue,
+                                  value: loadingProgress.expectedTotalBytes != null
+                                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                                      : null,
+                                ),
+                              );
+                            },
+                            errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                              return const Center(child: Text('Failed to load image.'));
+                            },
+                          ),
+                        ) : const SizedBox(),
                       ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Text(
-                              widget.topNews['articles'][index]['title'],
-                              textAlign: TextAlign.start,
-                              style: GoogleFonts.titilliumWeb(
-                                  color: Colors.grey[200],
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w600
+                      Container(
+                        width: double.infinity,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[900]!.withOpacity(0.6),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text(
+                                widget.topNews['articles'][index]['title'],
+                                textAlign: TextAlign.start,
+                                style: GoogleFonts.titilliumWeb(
+                                    color: Colors.grey[200],
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w600
+                                ),
                               ),
                             ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(left: 12.0,bottom: 8),
-                                child: SizedBox(
-                                  width: 120,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 12.0,bottom: 8),
+                                  child: SizedBox(
+                                    width: 120,
+                                    child: Text(
+                                      widget.topNews['articles'][index]['author'] ?? "",
+                                      overflow: TextOverflow.ellipsis,
+                                      style: GoogleFonts.titilliumWeb(
+                                        fontSize: 11,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.grey[200],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 12.0,bottom: 8),
                                   child: Text(
-                                    widget.topNews['articles'][index]['author'] ?? "",
-                                    overflow: TextOverflow.ellipsis,
+                                      widget.topNews['articles'][index]['publishedAt'] != null ? DateFormat('EEEE, d MMMM yyyy').format(
+                                      DateTime.parse(widget.topNews['articles'][index]['publishedAt'])
+                                    ) : "",
+                                    textAlign: TextAlign.end,
                                     style: GoogleFonts.titilliumWeb(
                                       fontSize: 11,
-                                      fontWeight: FontWeight.w500,
                                       color: Colors.grey[200],
                                     ),
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 12.0,bottom: 8),
-                                child: Text(
-                                    widget.topNews['articles'][index]['publishedAt'] != null ? DateFormat('EEEE, d MMMM yyyy').format(
-                                    DateTime.parse(widget.topNews['articles'][index]['publishedAt'])
-                                  ) : "",
-                                  textAlign: TextAlign.end,
-                                  style: GoogleFonts.titilliumWeb(
-                                    fontSize: 11,
-                                    color: Colors.grey[200],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             }
